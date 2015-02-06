a3e80b144c70cb8fc02b62195480263d5c079da9
datjeff <jeff91jr@gmail.com>
2015-02-05 19:31:58 -0500

    added basic http auth

--- server.go
+++ server.go
@@ -2,15 +2,21 @@ package main
 
 import(
 	"github.com/go-martini/martini"
+	"github.com/martini-contrib/auth"
 	"SecretService/Models"
 	"SecretService/Controllers"
 ) 
 
 func main() {
-  martiniClassic := martini.Classic()
-        secretThingCollection := Models.NewSecretThingCollection()
-        Controllers.RegisterRestfulService(secretThingCollection, martiniClassic)
-        martiniClassic.Run()
+  	martiniClassic := martini.Classic()
+	
+	secretThingCollection := Models.NewSecretThingCollection()
+	
+	Controllers.RegisterRestfulService(secretThingCollection, martiniClassic)
+
+	martiniClassic.Use(auth.BasicFunc(Controllers.IsAuthorized))
+
+	martiniClassic.Run()
 }
 
 
