9598a16500914c1adbfe0b4a8336652074ac46f2
Isaac Cummings <Cummingsi1993@gmail.com>
2015-02-06 14:56:37 -0500

    data access

--- server.go
+++ server.go
@@ -5,16 +5,34 @@ import(
 	"github.com/martini-contrib/auth"
 	"github.com/cummingsi1993@gmail.com/SecretService/Models"
 	"github.com/cummingsi1993@gmail.com/SecretService/Controllers"
+	//"encoding/json"
+	"net/http"
 ) 
 
 func main() {
   	martiniClassic := martini.Classic()
-	
+	//martiniClassic.RunOnAddr(":3000")
 	secretThingCollection := Models.NewSecretThingCollection()
+	_ = secretThingCollection //temporarily
 	
-	Controllers.RegisterRestfulService(secretThingCollection, martiniClassic)
+	//Controllers.RegisterRestfulService(secretThingCollection, martiniClassic)
 
 	martiniClassic.Use(auth.BasicFunc(Controllers.IsAuthorized))
+
+	martiniClassic.Get("/SecretThing/:key", func(params martini.Params) string {
+		//DataAccess.Get(new(CouchbaseDal), params[key])
+		return "woot"
+	})
+
+	martiniClassic.Put("/SecretThing/:key", func(params martini.Params, req *http.Request) string {
+		
+
+		return "ok"
+	})
+
+	martiniClassic.Get("/hello/:name", func(params martini.Params) string {
+	  return "Hello " + params["name"]
+	})
 
 	martiniClassic.Run()
 }
